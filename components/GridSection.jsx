0aa862ebd2eea7afcef674c01440445573558ac5
Aditya Rawat <69760948+adityarawat007@users.noreply.github.com>
2022-10-04 21:45:42 +0530

    added rounded corners

--- components/GridSection.jsx
+++ components/GridSection.jsx
@@ -1,20 +1,20 @@
 const GridSection = () => {
   return (
-    <section className="conatiner">
+    <section className="">
       <div className="py-10 md:py-20 ">
-        <div className="mx-auto flex flex-col items-center justify-center space-y-14  ">
+        <div className="conatiner lg:mx-14 flex flex-col items-center justify-center space-y-14  ">
           <h2 className="text-6xl font-medium">
             Empower your teams with Metronome
           </h2>
-          <div className="grid bg-green-600 lg:grid-cols-2 rounded-lg">
-            <div className="space-y-6 bg-op-green px-6 py-10 md:px-20 md:py-24 ">
+          <div className="grid  lg:grid-cols-2 ">
+            <div className="space-y-6 bg-op-green px-6 py-10 md:px-20 md:py-24 rounded-t-2xl md:rounded-t-2xl lg:rounded-t-none lg:rounded-tl-2xl  ">
               <img src="/grid-1.svg" alt="grid one image" />
               <div className="space-y-2">
                 <h6 className="text-sm font-medium  text-white md:text-lg">
                   ENGINEERING
                 </h6>
                 <h5 className="text-xl font-medium text-white md:text-2xl">
-                  Easy integration, easier maintenance{" "}
+                  Easy integration, easier maintenance
                 </h5>
               </div>
               <p className="text-md font-medium text-white md:text-xl">
@@ -26,14 +26,14 @@ const GridSection = () => {
                 experience.
               </p>
             </div>
-            <div className="space-y-6 bg-op-light-blue px-6 py-10 md:px-20 md:py-24">
+            <div className="space-y-6 bg-op-light-blue px-6 py-10 md:px-20 md:py-24 lg:rounded-tr-2xl">
               <img src="/grid-2.svg" alt="grid one image" />
               <div className="space-y-2">
                 <h6 className="text-sm font-medium   md:text-lg">
                   ENGINEERING
                 </h6>
                 <h5 className="text-xl font-medium  md:text-2xl">
-                  Easy integration, easier maintenance{" "}
+                  Easy integration, easier maintenance
                 </h5>
               </div>
               <p className="text-md font-medium  md:text-xl">
@@ -45,14 +45,14 @@ const GridSection = () => {
                 experience.
               </p>
             </div>
-            <div className="space-y-6 bg-op-light-mint px-6 py-10 md:px-20 md:py-24">
+            <div className="space-y-6 bg-op-light-mint px-6 py-10 md:px-20 md:py-24 lg:rounded-bl-2xl">
               <img src="/grid-3.svg" alt="grid one image" />
               <div className="space-y-2">
                 <h6 className="text-sm font-medium  md:text-lg">
                   ENGINEERING
                 </h6>
                 <h5 className="text-xl font-medium  md:text-2xl">
-                  Easy integration, easier maintenance{" "}
+                  Easy integration, easier maintenance
                 </h5>
               </div>
               <p className="text-md font-medium  md:text-xl">
@@ -64,14 +64,14 @@ const GridSection = () => {
                 experience.
               </p>
             </div>
-            <div className="space-y-6 bg-op-clay px-6 py-10 md:px-20 md:py-24">
+            <div className="space-y-6 bg-op-clay px-6 py-10 md:px-20 md:py-24 rounded-b-2xl md:rounded-b-2xl lg:rounded-bl-none  lg:rounded-br-2xl">
               <img src="/grid-4.svg" alt="grid one image" />
               <div className="space-y-2">
                 <h6 className="text-sm font-medium   md:text-lg">
                   ENGINEERING
                 </h6>
                 <h5 className="text-xl font-medium  md:text-2xl">
-                  Easy integration, easier maintenance{" "}
+                  Easy integration, easier maintenance
                 </h5>
               </div>
               <p className="text-md font-medium  md:text-xl">
